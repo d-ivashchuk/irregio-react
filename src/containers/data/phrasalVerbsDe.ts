9a7a7e7b7998a447f7a0f7dc62f5ca295a295ef3
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-10-05 13:47:17 -0600

    Adding German phrasal verbs

---
+++ src/containers/data/phrasalVerbsDe.ts
@@ -0,0 +1,559 @@
+export default {
+  data: [
+    {
+      pVerb: "abhängen",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "achten",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "anfangen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "ankommen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "antworten",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich ärgern",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "aufhören",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "aufpassen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich aufregen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "ausgeben",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich bedanken",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich bedanken",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "beginnen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich bemühen",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "berichten",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich beschäftigen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich beschweren",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "bestehen",
+      prae: "aus",
+      kasus: "D"
+    },
+    {
+      pVerb: "bestehen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich beteiligen",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich bewerben",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich bewerben",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich beziehen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "bitten",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "danken",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "denken",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "diskutieren",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "einladen",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich entscheiden",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich entschließen",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich entschuldigen",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich entschuldigen",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "erfahren",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich erholen",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich erinnern",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "erkennen",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich erkundigen",
+      prae: "nach",
+      kasus: "D"
+    },
+    {
+      pVerb: "erschrecken",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "erzählen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "erzählen",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "fragen",
+      prae: "nach",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich freuen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich freuen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "gehen",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "gehören",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich gewöhnen",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "glauben",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "gratulieren",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "halten",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "halten",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich handeln",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "handeln",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "helfen",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "hindern",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "hoffen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich informieren",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich interessieren",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "klagen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "kämpfen",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "kommen",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich konzentrieren",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich kümmern",
+      prae: "um",
+      kasus: "A"
+    },
+    {
+      pVerb: "lachen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "leiden",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "leiden",
+      prae: "unter",
+      kasus: "A"
+    },
+    {
+      pVerb: "nachdenken",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "protestieren",
+      prae: "gegen",
+      kasus: "A"
+    },
+    {
+      pVerb: "rechnen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "reden",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "reden",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "riechen",
+      prae: "nach",
+      kasus: "D"
+    },
+    {
+      pVerb: "sagen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "sagen",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "schicken",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "schicken",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "schimpfen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "schmecken",
+      prae: "nach",
+      kasus: "D"
+    },
+    {
+      pVerb: "schreiben",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich schützen",
+      prae: "vor",
+      kasus: "D"
+    },
+    {
+      pVerb: "sein",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "sein",
+      prae: "gegen",
+      kasus: "A"
+    },
+    {
+      pVerb: "sorgen",
+      prae: "für",
+      kasus: "A"
+    },
+    {
+      pVerb: "sprechen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sprechen",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "sterben",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "streiten",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "streiten",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "teilnehmen",
+      prae: "an",
+      kasus: "D"
+    },
+    {
+      pVerb: "telefonieren",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich treffen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich treffen",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "überreden",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich unterhalten",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich unterhalten",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich verabreden",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich verabschieden",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "vergleichen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich verlassen",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich verlieben",
+      prae: "in",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich verstehen",
+      prae: "mit",
+      kasus: "D"
+    },
+    {
+      pVerb: "verstehen",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich vorbereiten",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "warnen",
+      prae: "vor",
+      kasus: "D"
+    },
+    {
+      pVerb: "warten",
+      prae: "auf",
+      kasus: "A"
+    },
+    {
+      pVerb: "sich wenden",
+      prae: "an",
+      kasus: "A"
+    },
+    {
+      pVerb: "werden",
+      prae: "zu",
+      kasus: "D"
+    },
+    {
+      pVerb: "wissen",
+      prae: "von",
+      kasus: "D"
+    },
+    {
+      pVerb: "sich wundern",
+      prae: "über",
+      kasus: "A"
+    },
+    {
+      pVerb: "zuschauen",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "zusehen",
+      prae: "bei",
+      kasus: "D"
+    },
+    {
+      pVerb: "zweifeln",
+      prae: "an",
+      kasus: "D"
+    }
+  ]
+};
